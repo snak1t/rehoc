7c2cd9892d25a6b19cec4f4ba05e9e972127fcd2
Ruslan Abramov <r.o.abramov@gmail.com>
2017-07-25 18:40:15 +0300

    fix

--- src/Validator.js
+++ src/Validator.js
@@ -2,30 +2,32 @@ const React = require('react')
 
 const withValidation = config => Component => {
   return class Validator extends React.Component {
-    state = config.reduce((state, { field, initialValue }) => {
-      return {
-        ...state,
-        [field]: {
-          value: initialValue || '',
-          errors: [],
-          status: { dirty: !!initialValue, valid: false }
-        }
-      }
-    }, {})
+    constructor(props) {
+      super(props)
+      this.state = config.reduce((state, { field, initialValue }) => {
+        return Object.assign({}, state, {
+          [field]: {
+            value: initialValue || '',
+            errors: [],
+            status: { dirty: !!initialValue, valid: false }
+          }
+        })
+      }, {})
+    }
 
     performAsyncValidation(rule, message, key, value) {
       const cb = result => {
         if (result) return
         this.setState(prevState => {
           const errors = [...prevState[key].errors, message]
-          const stateSlice = { ...prevState[key], errors }
-          return { ...prevState, [key]: stateSlice }
+          const stateSlice = Object.assign({}, prevState[key], { errors })
+          return Object.assign({}, prevState, { [key]: stateSlice })
         })
       }
       return rule(value, cb)
     }
 
-    checkForErrors = ({ key, value, isTarget, state }) => {
+    checkForErrors({ key, value, isTarget, state }) {
       if (state[key].status.dirty === false && !isTarget) return []
       const [{ validators }] = config.filter(confItem => confItem.field === key)
       if (typeof validators === 'undefined') {
@@ -54,50 +56,58 @@ const withValidation = config => Component => {
       return errors
     }
 
-    valueHandler = key => data => {
-      const value = data && data.target ? data.target.value : data
+    valueHandler(key) {
+      return data => {
+        const value = data && data.target ? data.target.value : data
 
-      config.forEach(({ field: stateKey }) => {
-        this.setState(prevState => {
-          const isTarget = stateKey === key
-          const updatedValue = isTarget ? value : prevState[stateKey].value
-          const errors = this.checkForErrors({
-            key: stateKey,
-            value: updatedValue,
-            isTarget,
-            state: prevState
+        config.forEach(({ field: stateKey }) => {
+          this.setState(prevState => {
+            const isTarget = stateKey === key
+            const updatedValue = isTarget ? value : prevState[stateKey].value
+            const errors = this.checkForErrors({
+              key: stateKey,
+              value: updatedValue,
+              isTarget,
+              state: prevState
+            })
+            const stateSlice = Object.assign({}, prevState[stateKey], {
+              value: updatedValue,
+              errors,
+              status: {
+                dirty: prevState[stateKey].status.dirty || isTarget,
+                valid: errors.length === 0
+              }
+            })
+            return Object.assign({}, prevState, { [stateKey]: stateSlice })
           })
-          const stateSlice = {
-            ...prevState[stateKey],
-            value: updatedValue,
-            errors,
-            status: {
-              dirty: prevState[stateKey].status.dirty || isTarget,
-              valid: errors.length === 0
-            }
-          }
-          return { ...prevState, [stateKey]: stateSlice }
         })
-      })
+      }
     }
 
-    isInvalidField = field => !field.status.valid || !field.status.dirty
+    isInvalidField(field) {
+      return !field.status.valid || !field.status.dirty
+    }
 
-    prepareProps = () => {
+    prepareProps() {
       const props = {}
       let valid = true
       for (let key in this.state) {
         if (this.isInvalidField(this.state[key])) {
           valid = false
         }
-        props[key] = { ...this.state[key], handler: this.valueHandler(key) }
+        props[key] = Object.assign({}, this.state[key], {
+          handler: this.valueHandler(key)
+        })
       }
       props.valid = valid
       return props
     }
 
     render() {
-      return <Component {...this.props} {...this.prepareProps()} />
+      return React.createElement(
+        Component,
+        Object.assign({}, this.props, this.prepareProps())
+      )
     }
   }
 }

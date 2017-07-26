670bb0ad3ab9fe1eefc4e04d9cf3cb192c69005a
Ruslan Abramov <r.o.abramov@gmail.com>
2017-07-26 13:19:12 +0300

    Add test for main Component

---
+++ src/__test__/Validator.spec.js
@@ -0,0 +1,103 @@
+const React = require('react')
+const { shallow, mount } = require('enzyme')
+const withValidation = require('../Validator')
+
+const DummyForm = ({ login, asyncField }) =>
+  <form>
+    <input
+      type="text"
+      className="loginInput"
+      value={login.value}
+      onChange={login.handler}
+    />
+    <input
+      type="text"
+      className="asyncInput"
+      value={asyncField.value}
+      onChange={asyncField.handler}
+    />
+  </form>
+
+const msg = 'Name must be John'
+const validationConfig = [
+  {
+    field: 'login',
+    validators: [{ rule: name => name === 'John', message: msg }]
+  },
+  {
+    field: 'asyncField',
+    validators: [
+      {
+        rule: (value, done) => setTimeout(() => done(value), 100),
+        message: 'AsyncMsg',
+        async: true
+      }
+    ]
+  }
+]
+
+describe('Validator', () => {
+  it('should exist', () => {
+    const Component = withValidation(validationConfig)(DummyForm)
+    const component = mount(<Component />)
+    const testForm = component.find(DummyForm)
+    expect(component.exists()).toEqual(true)
+    expect(testForm.props()).toMatchSnapshot()
+  })
+
+  it('should update a string value', () => {
+    const Component = withValidation(validationConfig)(DummyForm)
+    const component = mount(<Component />)
+    const testForm = component.find(DummyForm)
+    const input = component.find('.loginInput')
+    input.simulate('change', { target: { value: 'Sam' } })
+    expect(testForm.props().login.value).toEqual('Sam')
+  })
+
+  it('should mark a field as dirty if changed', () => {
+    const Component = withValidation(validationConfig)(DummyForm)
+    const component = mount(<Component />)
+    const testForm = component.find(DummyForm)
+    const input = component.find('.loginInput')
+    input.simulate('change', { target: { value: 'Sam' } })
+    expect(testForm.props().login.status.dirty).toEqual(true)
+  })
+
+  it('should mark a field as valid if provided with valid value', () => {
+    const Component = withValidation(validationConfig)(DummyForm)
+    const component = mount(<Component />)
+    const testForm = component.find(DummyForm)
+    const input = component.find('.loginInput')
+    input.simulate('change', { target: { value: 'Sam' } })
+    expect(testForm.props().login.status.valid).toEqual(false)
+    input.simulate('change', { target: { value: 'John' } })
+    expect(testForm.props().login.status.valid).toEqual(true)
+  })
+
+  it('should provide an error array if specified value is incorrect', () => {
+    const Component = withValidation(validationConfig)(DummyForm)
+    const component = mount(<Component />)
+    const testForm = component.find(DummyForm)
+    const input = component.find('.loginInput')
+    input.simulate('change', { target: { value: 'Sam' } })
+    expect(testForm.props().login.status.valid).toEqual(false)
+    expect(testForm.props().login.errors).toBeInstanceOf(Array)
+    expect(testForm.props().login.errors.length).toEqual(1)
+    expect(testForm.props().login.errors[0]).toEqual(msg)
+  })
+
+  it('should also deal with async validations', () => {
+    const waiter = () =>
+      new Promise((res, rej) => {
+        setTimeout(() => res(), 500)
+      })
+    const Component = withValidation(validationConfig)(DummyForm)
+    const component = mount(<Component />)
+    const testForm = component.find(DummyForm)
+    const input = component.find('.asyncInput')
+    input.simulate('change', { target: { value: true } })
+    return waiter().then(() => {
+      expect(testForm.props().asyncField.status.valid).toEqual(true)
+    })
+  })
+})
